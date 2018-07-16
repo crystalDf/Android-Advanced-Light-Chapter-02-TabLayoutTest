ee02515e2c575cf2b31fbbf2123c7c2ab0501cbb
crystalDf <chendong333@gmail.com>
2018-07-16 23:41:37 +0800

    TabLayoutTest

--- app/src/main/java/com/star/tablayouttest/TabLayoutActivity.java
+++ app/src/main/java/com/star/tablayouttest/TabLayoutActivity.java
@@ -1,13 +1,47 @@
 package com.star.tablayouttest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TabLayoutActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab_layout);
+
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mViewPager = findViewById(R.id.viewpager);
+
+        initViewPager();
+    }
+
+    private void initViewPager() {
+
+        mTabLayout = findViewById(R.id.tab_layout);
+
+        List<String> titles = new ArrayList<>();
+
+        titles.add("精选");
+        titles.add("体育");
+        titles.add("巴萨");
+        titles.add("购物");
+
+        for (String title : titles) {
+
+            mTabLayout.addTab(mTabLayout.newTab().setText(title));
+        }
     }
 }
